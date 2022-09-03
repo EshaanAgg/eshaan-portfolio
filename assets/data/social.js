268d7b7766a29315655522420279ff7311c5f56b
Eshaan Aggarwal <96648934+EshaanAgg@users.noreply.github.com>
2022-09-03 13:35:27 +0530

    Remove socials

--- assets/data/social.js
+++ assets/data/social.js
@@ -1,8 +1,8 @@
 const social = {
     github: "https://github.com/eshaanagg",
     linkedin : "https://linkedin.com/in/eshaan-aggarwal",
-    twitter: "https://twitter.com/eshaanagg",
-    instagram: "https://instagram.com/eshhh.aan",
+    twitter: "https://twitter.com/",
+    instagram: "https://instagram.com/",
     mail : "mailto : eshaanagg@gmail.com"
 }
 
